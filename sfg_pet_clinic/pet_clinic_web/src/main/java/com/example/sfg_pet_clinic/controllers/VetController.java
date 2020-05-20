501becb29feb518403eccc0a7d47e0bdacc61639
Gayal Balasooriya <baigayal01@gmail.com>
2020-05-20 19:20:24 +0530

    create pages and controllers

---
+++ sfg_pet_clinic/pet_clinic_web/src/main/java/com/example/sfg_pet_clinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package com.example.sfg_pet_clinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String ListVets() {
+        return "vets/index";
+    }
+}
